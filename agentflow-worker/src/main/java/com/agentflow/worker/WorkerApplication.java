a86952d1fece84b2559dde03a96f9e1d13bb0020
Cong <congfanpu@gmail.com>
2026-07-11 23:05:11 +0800

    feat: Maven 多模块骨架(common/server/worker)

---
+++ agentflow-worker/src/main/java/com/agentflow/worker/WorkerApplication.java
@@ -0,0 +1,15 @@
+package com.agentflow.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class WorkerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(WorkerApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+    }
+}
